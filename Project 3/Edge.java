dc44c7c28e8557340c53611ffd8129e9fd6b3365
Alec Flanigan <iA1ec@yahoo.com>
2017-04-25 15:54:37 -0400

    Added Vertex and Edge class and updated Shop class

---
+++ Project 3/Edge.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Edge here.
+ *
+ * @author Alec Flanigan
+ * @version 4/25/2017
+ */
+public class Edge
+{
+    private Vertex start, end;
+    private int distance;
+    
+    public Edge( Vertex s, Vertex e ) {
+        this.start = s;
+        this.end = e;
+        int xDis = (int)Math.abs( s.getLocation().getX() - e.getLocation().getX() );
+        int yDis = (int)Math.abs( s.getLocation().getY() - e.getLocation().getY() );
+        this.distance = xDis + yDis;
+    }
+    
+    public Vertex getStart() {
+        return this.start;
+    }
+    
+    public Vertex getEnd() {
+        return this.end;
+    }
+    
+    public int getDistance() {
+        return this.distance;
+    }
+}
--- Project 3/Shop.java
+++ Project 3/Shop.java
@@ -7,20 +7,17 @@ import java.awt.Point;
  * @author Alec Flanigan
  * @version 4/20/2017
  */
-public class Shop
+public class Shop extends Vertex
 {
-    private String id; // unique id of the shop
-    private Point location; // first position is x coord, second postition is y coord
-    private ArrayList<Integer> supplyList; // set of supplies the shop needs
+    private ArrayList< Cargo > supplyList; // set of supplies the shop needs
     
     /**
      * Constructor to create a shop with the given id and point of location. Initializes the ArrayList of supplies as empty.
      * @param anId shop's unique id
      * @param coords shop location
      */
-    public Shop( String anId, Point coords ) {
-        this.id = anId;
-        this.location = coords;
+    public Shop( Point aLocation, String anId ) {
+        super( anId, aLocation );
         this.supplyList = new ArrayList<Integer>();
     }
     
@@ -30,14 +27,28 @@ public class Shop
      * @param coords shop location
      * @param supplyList array of the shop's needed supplies
      */
-    public Shop( String anId, Point coords, int[] supplies )
+    public Shop( String anId, Point aLocation, int[] supplies )
     {
-        this.id = anId;
-        this.location = coords;
+        super( anId, aLocation );
         this.supplyList = new ArrayList<Integer>();
         
         for ( int s : supplies )
-            this.supplyList.add( new Integer( s ) );
+            this.supplyList.add( new Cargo( this, s ) );
+    }
+    
+    /**
+     * Constructor to create a shop with the given id, point of location, and list of supplies. Adds each position in the supplies array as an individual element in the local supplies ArrayList
+     * @param anId shop's unique id
+     * @param coords shop location
+     * @param supplyList array of the shop's needed supplies in string format
+     */
+    public Shop( String anId, Point aLocation, String[] supplies )
+    {
+        super( anId, aLocation );
+        this.supplyList = new ArrayList<Integer>();
+        
+        for ( String s : supplies )
+            this.supplyList.add( new Cargo( this, Integer.parseInt( s ) ) );
     }
     
     /**
@@ -45,7 +56,7 @@ public class Shop
      * @param supplies weight of added supplies
      */
     public void addSupplies( int supplies ) {
-        this.supplyList.add( supplies );
+        this.supplyList.add( new Cargo( this, supplies ) );
     }
     
     /**
@@ -54,6 +65,13 @@ public class Shop
      */
     public void addSupplies( int[] supplies ) {
         for ( int s : supplies )
-            this.supplyList.add( s );
+            this.supplyList.add( new Cargo( this, s ) );
+    }
+    
+    public void removeSupplies( Cargo c ) {
+        for ( int i = 0; i < this.supplyList.size(); i++ ) {
+            if ( this.supplyList.get( i ).equals( c ) )
+                this.supplyList.remove( i );
+        }
     }
 }
